338df6b8db075f09289ef7f4510c2a469941e0e2
Eddy Klammer <eddy.klammer@mail.de>
2021-11-03 15:56:49 +0100

    initialisierung

---
+++ src/main/java/de4/webtech/Webtech2021Application.java
@@ -0,0 +1,13 @@
+package de4.webtech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Webtech2021Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Webtech2021Application.class, args);
+	}
+
+}
---
+++ src/test/java/de4/webtech/Webtech2021ApplicationTests.java
@@ -0,0 +1,13 @@
+package de4.webtech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Webtech2021ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
